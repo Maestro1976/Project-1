b861317b2e3a8b2e2c0ed376659aa59c88a2317e
Maestro <3086963@gmail.com>
2020-03-12 12:58:58 +0300

    Оставил чистую функцию, остальное удалил

--- Java/Level1.java
+++ Java/Level1.java
@@ -1,50 +1,18 @@
-package com.company;
 import java.util.*;
 public class Level1
 {
     public static long squirrel(int N)
     {
         long result = 1;
+        long sum = 0;
         for (int i = 1; i <= N; i++)
         {
             result *= i;
         }
-        return result;
-    }
-
-    public static long quantityEmeralds(long factorial)
-    {
-        long sum = 0;
-        for (long i = factorial; i > 0; i = i / 10)
+        for (long i = result; i > 0; i = i / 10)
         {
             sum = i % 10;
         }
         return sum;
-    }
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int N;
-        long factorial;
-        long emeralds;
-        System.out.println("Введите целое неотрицательное число");
-        try
-        {
-            N = sc.nextInt();
-            if (N > 0)
-            {
-                factorial = squirrel(N);
-                emeralds = quantityEmeralds(factorial);
-                System.out.println("Количество изумрудов = " + emeralds);
-            }
-            else
-            {
-                System.err.println("Ошибка ввода! Число должно быть больше нуля!");
-            }
-        }
-        catch (InputMismatchException e)
-        {
-            System.err.println("Ошибка ввода! Введите целое число");
-        }
     }
 }
